3c4a2d2554488e67a9d486e8589696d14553fc09
AnnaKurashko <kuzann88@mail.ru>
2022-10-23 22:21:41 +0700

    Добавила стили лайкам

--- scripts/card.js
+++ scripts/card.js
@@ -11,8 +11,8 @@ class Card {
         const cardName = this.element.querySelector('.card__title');
         const cardImg = this.element.querySelector('.card__img');
         const cardLike = this.element.querySelector('.card__like');
-        if(!this._data.favourite){
-            cardLike.remove();
+        if(this._data.favourite){
+            cardLike.classList.add('card__like-active');
         }
         cardName.textContent = this._data.name;
         cardImg.src = this._data.img_link;
